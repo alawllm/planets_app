91e5a38616b2982d65097516783a898b218cfd30
alawllm <alicja.willam@gmail.com>
2023-03-31 14:08:29 +0200

    basic express structure

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-import axios from 'axios';
 
 
-
+//parsing data in order to populate the body
+app.use(express.urlencoded({ extended: true }))
+//serving static files
+app.use(express.static(path.join(__dirname, '/public')))
+//views directory - for ejs files
 app.set('views', path.join(__dirname, '/views'))
+//enabling ejs
 app.set('view engine', 'ejs')
 
+
+const planets = ['pluto', 'saturn', 'earth', 'mars', 'jupiter']
+
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('home', { planets })
 })
 
+app.post('/', (req, res) => {
+    res.send('post request!!!')
+})
 
-// const options = {
-//     method: 'GET',
-//     url: 'https://planets-by-api-ninjas.p.rapidapi.com/v1/planets',
-//     params: { name: 'Mars' },
-//     headers: {
-//         'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-//         'X-RapidAPI-Host': 'planets-by-api-ninjas.p.rapidapi.com'
-//     }
-// };
-// axios.request(options).then(function (response) {
-//     console.log(response.data);
-// }).catch(function (error) {
-//     console.error(error);
-// });
+app.get('/planets', (req, res) => {
+    res.render('planets')
+})
 
 app.listen(8080, () => {
     console.log('listening on port 8080')
