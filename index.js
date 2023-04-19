356707e38cbc242c91088c367edd240393b874c8
alawllm <alicja.willam@gmail.com>
2023-04-19 14:48:45 +0200

    fixed boilerplate issue, divided css files

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const ejsMate = require('ejs-mate')
 const path = require('path')
 const axios = require('axios');
 const key = require('./.env')
@@ -11,8 +10,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '/public')))
 //views directory - for ejs files
 app.set('views', path.join(__dirname, '/views'))
-//enabling ejs-mate: serving partials, include
-app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 // routing 
 app.get('/', (req, res) => {
