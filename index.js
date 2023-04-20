10984c155cdcc2d5b00349459463ebb1d61c2364
alawllm <alicja.willam@gmail.com>
2023-04-20 10:41:42 +0200

    fixed bug in error

--- index.js
+++ index.js
@@ -36,12 +36,34 @@ app.get('/planets', catchAsync(async (req, res) => {
     let src = await axios.request(options).then(function (response) {
         return response.data[0]
     })
-    if (error) {
+    if (!src) {
         res.render('notfound')
     }
 
     res.render('planets', { planetName, src })
 }))
+
+//random route 'i'm feeling lucky'
+// app.get('/randomplanet', catchAsync(async (req, res) => {
+//     // const {planetName} = ??
+//     const options = {
+//         method: 'GET',
+//         url: 'https://planets-by-api-ninjas.p.rapidapi.com/v1/planets',
+//         params: { name: planetName },
+//         headers: {
+//             'X-RapidAPI-Key': key,
+//             'X-RapidAPI-Host': 'planets-by-api-ninjas.p.rapidapi.com'
+//         }
+//     }
+//     let src = await axios.request(options).then(function (response) {
+//         return response.data[0]
+//     })
+//     if (error) {
+//         res.render('notfound')
+//     }
+
+//     res.render('planets', { planetName, src })
+// }))
 
 app.all('*', (req, res, next) => {
     res.render('notfound')
